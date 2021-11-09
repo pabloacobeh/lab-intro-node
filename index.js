04dd0b9c34a8d0ca1c76f5490e952e39c50f6a54
Pablo Acosta <pabloacobeh@gmail.com>
2021-11-09 14:25:32 -0500

    all done

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length += 1;
+    return this.items.sort((a, b) => {
+      return a - b;
+    });
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items.indexOf(pos) in this.items) {
+      return this.items.indexOf(pos);
+    }
+    throw new Error("OutOfBounds");
+  }
 
-  max() {}
+  max() {
+    if (this.items[this.items.length - 1]) {
+      return this.items[this.items.length - 1];
+    }
+    throw new Error("EmptySortedList");
+  }
 
-  min() {}
+  min() {
+    if (this.items[0]) {
+      return this.items[0];
+    }
+    throw new Error("EmptySortedList");
+  }
 
-  sum() {}
+  sum() {
+    return this.items.reduce((a, b) => {
+      return a + b;
+    }, 0);
+  }
 
-  avg() {}
+  avg() {
+    if (this.sum() / this.length) {
+      return this.sum() / this.length;
+    }
+    throw new Error("EmptySortedList");
+  }
 }
 
 module.exports = SortedList;
